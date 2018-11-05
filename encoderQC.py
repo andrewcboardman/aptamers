2bde42ca1377886710f9ac0ea1adc859ebd4a14b
Andrew <acb95@cam.ac.uk>
2018-11-05 13:50:22 +0000

    improved QC

--- encoderQC.py
+++ encoderQC.py
@@ -1,6 +1,37 @@
 import numpy as np
 from Bio import SeqIO
-import pickle
+
+input_file = 'test.fastq'
+score_cutoff = 15
+
+
+# Count number of reads
+count = 0
+for rec in SeqIO.parse(input_file, "fastq"):
+	count += 1
+print('{} total reads'.format(count))
+
+# Filter for any bases which have quality lower than a cutoff
+good_reads = (rec for rec in \
+	SeqIO.parse(input_file, "fastq") \
+	if min(rec.letter_annotations["phred_quality"]) >= score_cutoff)
+count = SeqIO.write(good_reads, "test_filter.fastq", "fastq")
+print("Saved {} reads".format(count))
+
+# 
+
+
+def SeqEncoder(seqfile):
+	# Import reads from file
+	with open('output.txt','w') as fid:
+		for seqline in SeqIO.parse(seqfile,'fastq'):
+
+			# encode each read as a binary data vector
+			codedline = encode(str(seqline))
+			fid.write(codedline+'\n')
+
+
+
 def encode(seq):
 	# Split up sequence into chars
 	chars = np.array(list(seq),dtype=str)
@@ -9,10 +40,4 @@ def encode(seq):
 	# Join vector to string
 	return np.array2string(boole)
 
-def SeqEncoder(seqfile):
-	# Import reads from file
-	with open('output.txt','w') as fid:
-		for seqline in SeqIO.parse(seqfile,'fastq'):
-			# encode each read as a binary data vector
-			codedline = encode(str(seqline))
-			fid.write(codedline+'\n')
+
