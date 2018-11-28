4fe278f5050b832b681efe93319b4bdc6c29c707
Andrew <acb95@cam.ac.uk>
2018-11-28 14:30:40 +0000

    frameshifts sample matrices to ruin your day

---
+++ shift_samples.py
@@ -0,0 +1,36 @@
+import numpy as np
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--infile', type=str, action='store', dest='infile',	help='Flag for original model')
+parser.add_argument('-o', '--outfile', type=str, action='store', dest='outfile', help='Flag for output files')
+args = parser.parse_args()
+
+def shift_sample(seq,shift):
+	if shift == 0:
+		return seq
+	else:
+		return np.concatenate((seq[shift:,...],seq[:shift,...]))
+
+# load sample metadata
+L, Nc, ns, nb, nw, b = np.genfromtxt(f'../test_data/output_{args.infile}/{args.outfile}/samples_inf.txt',dtype='int32')
+# Count samples
+n_samples = Nc*ns//nw
+# load samples
+samples = np.memmap(f'../test_data/output_{args.infile}/{args.outfile}/samples.npy',mode='r',dtype=int,shape=(n_samples,L,3))
+# Make new directory
+if not os.path.isdir(f'../test_data/output_{args.infile}/{args.outfile}_shifted'):
+	os.mkdir(f'../test_data/output_{args.infile}/{args.outfile}_shifted')
+# Create an array to write shifted samples into
+samples_shifted = np.memmap(f'../test_data/output_{args.infile}/{args.outfile}_shifted/samples.npy',mode='w+',dtype=int,shape=(n_samples,L,3))
+# Copy metadata
+np.savetxt(f'../test_data/output_{args.infile}/{args.outfile}_shifted/samples_inf.txt',(L,Nc,ns,nb,nw,b))
+# Generate random shifts
+shifts = np.random.randint(-5,6,size=n_samples)
+print(shifts.size)
+# shift each sample by a random amount
+for i in range(n_samples):
+	samples_shifted[i,...] = shift_sample(samples[i,...],shifts[i])
+
+
