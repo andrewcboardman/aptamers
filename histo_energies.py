found a bug with int overflow which is potentially the cause of all human suffering

histo_energies.py
@@ -3,8 +3,9 @@ import argparse
 import numpy as np
 
 def plot(E1,E2):
-	plt.hist(E1,density=True,color='blue',alpha=0.5)
-	plt.hist(energies_rand,density=True,color='red',alpha=0.5)
+	plt.switch_backend('TkAgg')
+	plt.hist(E1,density=True,color='blue',alpha=0.5,bins=50)
+	plt.hist(E2,density=True,color='red',alpha=0.5,bins=50)
 	plt.xlabel('Ising energy of sequence')
 	plt.ylabel('Frequency')
 	plt.show()
@@ -21,5 +22,5 @@ def main():
 	energies_rand = np.genfromtxt(args.path+args.infile2)
 	plot(energies,energies_rand)
 
-if __name__ = '__main__':
+if __name__ == '__main__':
 	main()

mf_potts_train.py
@@ -2,21 +2,26 @@ import argparse
 from encode_pysster import encode
 from mf_potts import mf_potts
 from histo_energies import plot
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--infile', type=str, action='store', dest='infile',	help='Input filename')
-parser.add_argument('-o', '--outfile', type=str, action='store', dest='infile',	help='Output filename')
+parser.add_argument('-o', '--outfile', type=str, action='store', dest='outfile',	help='Output filename')
 args = parser.parse_args()
 
-data, params = encode(args.infile)
+data, params = encode([args.infile])
 model = mf_potts(data,params)
-mf_potts.data.train_val_test_split(0.9,0.1)
+model.data.train_val_test_split(0.9,0.1)
 
-print('Imported data. Infering model...')
+print('Imported data. Inferring model coefficients...')
 J_mf,h_mf = model.train()
 
 print('Inference complete, saving coefficients...')
-np.savetxt(args.outfile,J_mf)
+np.savetxt(args.outfile,np.concatenate((J_mf,h_mf),axis=0))
 
 test_energies,random_energies = model.test()
+
+print(test_energies.shape,random_energies.shape)
+print(test_energies)
+print(random_energies)
 plot(test_energies,random_energies)

shift_samples.py
@@ -1,37 +1,26 @@
+from Bio import SeqIO
+from Bio import SeqRecord
+import os
 import numpy as np
 import argparse
-import os
+def random_base(n):
+	return ''.join(np.array(('A','C','G','T'))[np.random.randint(4,size=abs(n))])
+def shift(seqrecord,n_shift):
+	if n_shift == 0:
+		return seqrecord
+	elif n_shift > 0:
+		return random_base(n_shift) + seqrecord[:-n_shift]
+	else:
+		return seqrecord[(-n_shift):] + random_base((n_shift))
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i', '--infile', type=str, action='store', dest='infile',	help='Flag for original model')
-parser.add_argument('-o', '--outfile', type=str, action='store', dest='outfile', help='Flag for output files')
-parser.add_argument('-s', '--shift_length', type=int, action='store', dest='shift_length', help='Max number of bases to shift samples by')
+parser.add_argument('-i', '--infile', type=str, action='store', dest='infile',	help='Input file in FASTA format')
+parser.add_argument('-o', '--outfile', type=str, action='store', dest='outfile', help='Output file in FASTA format')
+parser.add_argument('-s', '--shift_length', type=int, action='store', dest='shift_length', help='Number of bases to shift samples by')
 args = parser.parse_args()
 
-def shift_sample(seq,shift):
-	if shift == 0:
-		return seq
-	else:
-		return np.concatenate((seq[shift:,...],seq[:shift,...]))
-
-# load sample metadata
-L, Nc, ns, nb, nw, b = np.genfromtxt(f'../test_data/output_{args.infile}/{args.outfile}/samples_inf.txt',dtype='int32')
-# Count samples
-n_samples = Nc*ns//nw
-# load samples
-samples = np.memmap(f'../test_data/output_{args.infile}/{args.outfile}/samples.npy',mode='r',dtype=int,shape=(n_samples,L,3))
-# Make new directory
-if not os.path.isdir(f'../test_data/output_{args.infile}/{args.outfile}_shifted_{args.shift_length}'):
-	os.mkdir(f'../test_data/output_{args.infile}/{args.outfile}_shifted_{args.shift_length}')
-# Create an array to write shifted samples into
-samples_shifted = np.memmap(f'../test_data/output_{args.infile}/{args.outfile}_shifted_{args.shift_length}/samples.npy',mode='w+',dtype=int,shape=(n_samples,L,3))
-# Copy metadata
-np.savetxt(f'../test_data/output_{args.infile}/{args.outfile}_shifted_{args.shift_length}/samples_inf.txt',(L,Nc,ns,nb,nw,b))
-# Generate random shifts
-shifts = np.random.randint(-args.shift_length,args.shift_length+1,size=n_samples)
-print(shifts.size)
-# shift each sample by a random amount
-for i in range(n_samples):
-	samples_shifted[i,...] = shift_sample(samples[i,...],shifts[i])
-
+input_seqs = SeqIO.parse(args.infile,'fasta')
+output_seqs = (shift(seq,args.shift_length) for seq in input_seqs)
+with open(args.outfile, "w") as output_handle:
+    SeqIO.write(output_seqs, output_handle, "fasta")
 